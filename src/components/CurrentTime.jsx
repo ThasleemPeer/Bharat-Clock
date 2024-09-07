ac15e732412d14a8fa78cc92c54c945cb9972988
ThasleemPeer <thasleempeer361@gmail.com>
2024-09-07 23:09:28 +0530

    Modfied Code

--- src/components/CurrentTime.jsx
+++ src/components/CurrentTime.jsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+import { useState } from "react";
 let CurrentTime =()=>{
-    let time=new Date();
+    const [time,setTime]=useState(new Date());
+    useEffect(()=>{
+        console.log("interval has been set")
+        const intervalId=setInterval(()=>{
+            setTime(new Date());
+
+        },1000);
+        return ()=>{
+            clearInterval(intervalId);
+            console.log("cancelled the interval");
+        }
+
+    },[]);
     return <p  className="lead">
         This is the Current time: {time.toLocaleDateString() }-{" "}-{time.toLocaleTimeString()}
         </p>
